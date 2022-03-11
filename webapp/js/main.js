758e74c618cb784071930e9a6df65f7b3f66f1d0
meamritsagar <meamrit.sgr@gmail.com>
2022-03-11 10:25:36 -0600

    commin

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -7,16 +7,19 @@
 import { Person } from "./model/person.js";
 import { Employee } from "./model/employee.js";
 
-const persons = [
-    new Person("Ana Smith", new Date(1998, 11, 15)),
-    new Person("Bob Jone", new Date(1945, 10, 16)),
-    new Person("Carlos Slim Helu", new Date(1976, 8, 24))
-];
+// Avoid polluting the global scope
+(function() {
+    const persons = [
+        new Person("Ana Smith", new Date(1998, 11, 15)),
+        new Person("Bob Jone", new Date(1945, 10, 16)),
+        new Person("Carlos Slim Helu", new Date(1976, 8, 24))
+    ];
 
-console.log("Assignment 10: Question 1");
-persons.forEach(p => console.log(p.toString()));
+    console.log("Assignment 10: Question 1");
+    persons.forEach(p => console.log(p.toString()));
 
-const jim = new Employee("Jim Hanson", new Date(1998, 11, 15), 245990, new Date());
+    const jim = new Employee("Jim Hanson", new Date(1998, 11, 15), 245990, new Date());
 
-console.log("Assignment 10: Question 2");
-console.log(jim.doJob("Software Engineer"));
+    console.log("Assignment 10: Question 2");
+    console.log(jim.doJob("Software Engineer"));
+})();
